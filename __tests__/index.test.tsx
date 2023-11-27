a45d597a87b9f7ea22dd8a68210f4226765cc277
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2023-11-27 20:29:36 +0000

    feat: add simple routing example

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,17 +1,26 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Home from "@/pages/index";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+import { createMemoryHistory } from "history";
 
-    const heading = screen.getByRole('heading', {
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
       name: /welcome to next\.js!/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+  it("renders the navigation", () => {
+    render(<Home />);
+
+    const link = screen.getByRole("link", { name: /this page/i });
+    expect(link).toBeInTheDocument();
+  });
+});
