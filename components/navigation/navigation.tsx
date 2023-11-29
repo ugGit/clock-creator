8db2d562aa26ec562040c156ab0ad2fd2f439141
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2023-11-29 21:00:42 +0000

    feat: configure nextjs to allow images from external resources

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -6,6 +6,7 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 type NavigationItem = {
   name: string;
@@ -45,10 +46,12 @@ export default function Navigation() {
               </div>
               <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex flex-shrink-0 items-center'>
-                  <img
+                  <Image
                     className='h-8 w-auto'
                     src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
                     alt='Your Company'
+                    width={72}
+                    height={42}
                   />
                 </div>
                 <div className='hidden sm:ml-6 sm:block'>
@@ -87,10 +90,12 @@ export default function Navigation() {
                     <Menu.Button className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
                       <span className='absolute -inset-1.5' />
                       <span className='sr-only'>Open user menu</span>
-                      <img
+                      <Image
                         className='h-8 w-8 rounded-full'
                         src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                         alt=''
+                        width={72}
+                        height={72}
                       />
                     </Menu.Button>
                   </div>
---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        port: "",
+      },
+    ],
+  },
+};
