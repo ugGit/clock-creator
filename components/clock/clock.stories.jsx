eb5a5e245a3314871ebae3708e0791997d58ba93
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2023-12-04 16:17:56 +0000

    feat: create a basic story for the clock component

---
+++ components/clock/clock.stories.jsx
@@ -0,0 +1,13 @@
+import Clock from './clock';
+
+export default {
+  component: Clock,
+  title: 'Clock',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    font: 'serif',
+  },
+};
