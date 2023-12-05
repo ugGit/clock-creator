0d853900777bb4adbf38dd77e52b2ef6f2b08b29
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2023-12-05 11:20:59 +0100

    refactor(clock): add stories for different fonts

--- components/Clock/Clock.stories.tsx
+++ components/Clock/Clock.stories.tsx
@@ -8,7 +8,7 @@ const meta: Meta = {
   argTypes: {
     font: {
       options: ['serif', 'sans-serif', 'arial', 'monospace'],
-      control: { type: 'radio' },
+      control: { type: 'select' },
     },
   },
 };
@@ -17,8 +17,22 @@ export default meta
 
 type Story = StoryObj<typeof Clock>;
 
-export const Default : Story = {
+export const Default : Story = {};
+
+export const SansSerif : Story = {
   args: {
-    font: 'serif',
+    font: 'sans-serif',
+  },
+};
+
+export const Arial : Story = {
+  args: {
+    font: 'arial',
+  },
+};
+
+export const Monospace : Story = {
+  args: {
+    font: 'monospace',
   },
 };
